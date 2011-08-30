503e1f5311d4ac496055c0c9c6b340a28d8bcc9a
Dave Mallows <dave.mallows@gmail.com>
2011-08-30 13:32:54 +0100

    Fixed zooming

--- future/cursor.py
+++ future/cursor.py
@@ -7,11 +7,12 @@ import math
 import contextlib
 
 class Mover(object):
-    __slots__ = ('_dx', '_dy')
-
+    """Represent a direction for moving by a given amount."""
     def __init__(self, dx, dy):
         self._dx = dx
         self._dy = dy
+        self.__doc__ = "Return function f(d) that moves by d*({0}, {1})".\
+                format(self._dx, self._dy)
 
     def norm(self):
         s = 1.0 / math.hypot(self._dx, self._dy)
@@ -25,39 +26,148 @@ class Mover(object):
     # We need this to be a descriptor over a callable so we can find which
     # object did the calling. This avoids having to use getattr, which can be
     # used for much more powerful things now.
-    def __get__(self, obj, cls, type_=None):
-        return lambda d: obj.move(d*self._dx, d*self._dy)
+    def __get__(self, ins, owner):
+        if ins is None:
+            return self
+        else:
+            return lambda d: ins.move(d*self._dx, d*self._dy)
+
+    def __set__(self, instance, value):
+        raise AttributeError('Cannot set attribute')
     
     def __add__(self, other):
         return other.add(self._dx, self._dy)
 
+    def __repr__(self):
+        return 'lol'
+
+class ChangeView(object):
+    """A `descriptor` that removes much boilerplate"""
+    def __init__(self, spacename):
+        self._spacename = spacename
+        self.__doc__ = 'Transform this cursor to the {0} space'.\
+                        format(self._spacename)
+
+    def __get__(self, ins, owner):
+        if ins is None:
+            return self
+        else:
+            return ins._switch_space(ins[self._spacename])
+
+    def __set__(self, ins, val):
+        raise AttributeError('Attribute not settable')
+
+    def __repr__(self):
+        return "ChangeView(%r)" % self._spacename
+
 class Cursor(object):
+    # BEGIN Magic
+    # These are class members that represent a deferred relative motion.
+    # Somebody calls c.N(10), where c is an instance of cursor, and the cursor
+    # moves `north' by 10 units in the current space.
+
     N = north = up = Mover(0, 1)
     E = east = right = Mover(1, 0)
     S = south = down = Mover(0, -1)
     W = west = left = Mover(-1, 0)
+
     NE = (N + E).norm()
     NW = (N + W).norm()
     SE = (S + E).norm()
     SW = (S + W).norm()
 
-    def __init__(self, x, y, path=None):
-        self._x, self._y = x, y
+    box = ChangeView('box')
+    paper = ChangeView('paper')
+    absolute = ChangeView('absolute')
+    # END Magic 
+
+    def __init__(self, context, spaces, current, pos = (0,0), path=None):
+        self.pos = pos
+        self._context = context
+        self._spaces = spaces.copy()
+        self._current = current
+        # assert current in spaces.values()
         self._path = [] if path is None else path
+
+    def copy(self, cls=None):
+        cls = self.__class__ if cls is None else cls
+        return cls(self._context,
+                   self._spaces,
+                   self._current,
+                   self.pos,
+                   self._path)
+
+    # --------------
+    # Basic movement
+    # ==============
+    #
+    # Note that call is implemented to move to a given x, y coordinate pair.
+    # This is for the benefit of inline calling, and helps with pipelining.
+
+    def __call__(self, x, y):
+        """Move to given position x, y"""
+        self._angle = math.atan2(y-self._y, x - self._x)
+        self._x = x
+        self._y = y
+        return self
 
     @property
     def pos(self):
+        """The x, y position of the cursor"""
         return (self._x, self._y)
+
+    @pos.setter
+    def pos(self, pos):
+        self._x, self._y = pos
+
+    def degrees(self):
+        """Get the current angle"""
+        return (90 - math.degrees(self._angle)) % 360
+
+    def bearing(self, bearing):
+        """Set the current bearing"""
+        self._angle = math.radians(90 - bearing)
+        return self
+    
+    # Angle needs a getter and setter, since internally it's stored in radians.
+    # Show me somebody who thinks 
+    degrees = property(degrees, bearing)
 
     def move(self, dx, dy):
         self._angle = math.atan2(dy, dx)
         self._x += dx
         self._y += dy
         return self
-    
+
+    # Path manipulation
     @property
-    def angle(self):
-        return (90 - math.degrees(self._angle)) % 360
+    def to(self):
+        self._path.append(self.pos)
+        return self
+
+    def zoom(self):
+        a = self.copy().absolute(*self._path.pop())
+        b = self
+        
+        x1, y1 = a.box.pos
+        x2, y2 = b.box.pos
+        
+        cls = self['paper'].__class__
+        spaces = self._spaces.copy()
+        boxspace = spaces['box'].append(cls(x1, y1, x2, y2).inverse)
+
+        spaces['box'] = boxspace
+        self._spaces=spaces
+
+        x1, y1 = a.paper.pos
+        x2, y2 = b.paper.pos
+
+        dx = x2 - x1
+        dy = y2 - y1
+
+        spaces['paper'] = boxspace.append(cls(0,0,dx,dy))
+        return self
+
 
     @contextlib.contextmanager
     def clip(self):
@@ -67,11 +177,10 @@ class Cursor(object):
         >>> with cr.cursor(0,0).to(10,10).rect.clip as c:
         ...     c(-10,-10).to(10,10).line.draw()
         """
-        print "save"
-        #self._context.save()
+        self._context.save()
         yield
         print "restore"
-        #self._context.restore()
+        self._context.restore()
 
     def head(self, x):
         self._angle = math.radians(90 - x)
@@ -90,45 +199,31 @@ class Cursor(object):
         """Turn right (clockwise) by `dt` degrees"""
         self._angle -= math.radians(dt)
 
-    """
-    def sweep(self, dt):
-        'Sweep by `dt` counterclockwise using current point as pivot'
-        self._angle -= math.radians(dt)
+    def _switch_space(self, newSpace):
+        """Convert from one space into another"""
+        if newSpace is not self._current:
+            self.pos = newSpace.from_box(
+                self._current.to_box(self.pos))
+            self._current = newSpace
 
-    def curve_right(self, radius, dt):
-        t1 = self._angle
-        self.rt(dt)
-        t2 = self._angle
-
-    def curve_left(self, radius, dt):
-        self._angle += math.radians(dt)
-    """
-
-    @property
-    def to(self):
-        self._path.append(self.pos)
         return self
-
-    @property
-    def point(self, bearing):
-        return self
-
-    @property
-    def clone(self):
-        return Cursor(self._x, self._y)
 
     def __getitem__(self, key):
         try:
-            t = self._spaces[key]
-            return self._switch_space(
+            return self._spaces[key]
         except KeyError, e:
             raise KeyError("Space %r does not exist!" % key)
 
-    def __call__(self, x, y):
-        self._angle = math.atan2(y-self._y, x - self._x)
-        self._x = x
-        self._y = y
-        return self
+    def __setitem__(self, name, newSpace):
+        """Set the space"""
+        try:
+            if self[name] is self._current:
+                self._switch_space(newSpace)
+        except KeyError:
+            pass
+        
+        self._spaces[name] = newSpace
+
 
     def __repr__(self):
         return '<%s%r>' % (self.__class__.__name__, (self._x, self._y))
--- future/test_cursor.py
+++ future/test_cursor.py
@@ -1,15 +1,31 @@
 import unittest
-from cursor import Cursor
+import cursor
 import cPickle
+import sys
+from os.path import abspath
 
-class TestAngle(unittest.TestCase):
-    pass
+sys.path.append(abspath('..'))
+
+from crayon.spaces import Space2D, LinSpace2D, BoxSpace
+
+class MockContext(object):
+    def __getattr__(self, attr):
+        return self
+    def __call__(self, *args):
+        return self
 
 class TestCursor(unittest.TestCase):
+    def setUp(self):
+        spaces = {'paper':LinSpace2D(0,0,80,60),
+                  'absolute':LinSpace2D(0,0,800,600),
+                  'box':BoxSpace()}
+        self.c = cursor.Cursor(MockContext(), spaces, spaces['paper'] )
+
     def test_move(self):
         """Check that we can draw a 10 x 10 square and return at our
         starting position"""
-        c = Cursor(0,0)
+        c = self.c
+
         c.move(10,0)
         self.assertEqual((10, 0), c.pos)
         c.move(0,10)
@@ -20,42 +36,99 @@ class TestCursor(unittest.TestCase):
         self.assertEqual((0, 0), c.pos)
 
     def test_compass(self):
-        c = Cursor(0,0).to.N(10).to.E(10).to.S(10).to
+        c = self.c.copy()
+
+        c = c(0,0).to.N(10).to.E(10).to.S(10).to
         self.assertEqual(c._path, [(0,0), (0,10), (10,10), (10,0)])
-        c = Cursor(0,0).to.NW(10).to.NE(10).to.SE(10).to
-        c2 = Cursor(0,0).to.NE(10)
+
+        c = self.c.copy()
+        c(0,0).to.NW(10).to.NE(10).to.SE(10).to
+
+        c2 = self.c.copy()
+        c2(0,0).to.NE(10)
+
         self.assertEqual(c.pos, c2.pos)
 
     def test_copy(self):
-        c = Cursor(10, 10)
-        c2 = c.clone
+        c = self.c
+        c(10, 10)
+
+        c2 = c.copy()
         c2.move(-10, -10)
+
         self.assertEqual(c2.pos, (0,0))
         self.assertNotEqual(c2.pos, c.pos)
 
     def test_to(self):
-        c = Cursor(0,0)
+        c = self.c
+        c(0,0)
         self.assertEqual(c.to(10,10).to._path, [(0,0), (10,10)])
 
     def test_pickle(self):
-        c = Cursor(0,0).NW(10)
+        c = self.c.copy()
+        c = c(0,0).NW(10)
         c2 = cPickle.loads(cPickle.dumps(c))
         self.assertEqual(c.pos, c2.pos)
 
     def test_angle(self):
-        c = Cursor(0,0).to.NW(10).angle
-        print c
-
+        c = self.c
+        c(0,0).to.NW(10)
+        self.assertAlmostEqual(315, c.degrees)
 
     def test_bearing(self):
-        c = Cursor(0,0)
-        self.assertEqual(0, c(0,0).to.N(10).angle)
-        self.assertEqual(90, c(0,0).to.E(10).angle)
-        self.assertEqual(180, c(0,0).S(10).angle)
-        self.assertEqual(270, c(0,0).to.W(10).angle)
+        c = self.c
+
+        self.assertEqual(0, c(0,0).to.N(10).degrees)
+        self.assertEqual(90, c(0,0).to.E(10).degrees)
+        self.assertEqual(180, c(0,0).S(10).degrees)
+        self.assertEqual(270, c(0,0).to.W(10).degrees)
         
         for x in (11.8, 279.15, 87.3, 100.2):
-            self.assertAlmostEqual(x, c(0,0).head(x).angle)
+            self.assertAlmostEqual(x, c(0,0).bearing(x).degrees)
+
+    def test_spaces(self):
+        self.assertEquals(self.c(40,30).box.pos, (0.5,0.5))
+        self.assertEquals(self.c(0, 0).box.pos, (0,0))
+        self.assertEquals(self.c.box(1, 1).paper.pos, (80,60))
+
+    def test_item(self):
+        c = self.c.copy()
+        c(10,10)
+        paper = c['paper']
+        c['paper'] = c['absolute']
+
+        x, y = c.pos
+        self.assertAlmostEquals(x, 100)
+        self.assertAlmostEquals(y, 100)
+
+        c['paper'] = paper
+        x, y = c.pos
+        self.assertAlmostEquals(x, 10)
+        self.assertAlmostEquals(y, 10)
+
+    def test_subclassing(self):
+        class CursorDeluxe(cursor.Cursor):
+            plot = cursor.ChangeView('plot')
+        
+        c = self.c.copy(CursorDeluxe)
+        c['plot'] = LinSpace2D(50,50,100,100)
+        
+        self.assertAlmostEqual(
+            c.box(1,1).plot.pos, (100,100))
+
+        self.assertAlmostEqual(
+            c.box(0,0).plot.pos, (50,50))
+
+    def test_zoom(self):
+        c = self.c
+        c.paper(0,0).to(10,10).zoom()
+        x, y = c.box(1,1).paper.pos
+        self.assertAlmostEquals(x, 10)
+        self.assertAlmostEquals(y, 10)
+
+        x, y = c.box(1,1).absolute.pos
+        self.assertAlmostEquals(x, 100)
+        self.assertAlmostEquals(y, 100)
 
 if __name__=='__main__':
     unittest.main()
